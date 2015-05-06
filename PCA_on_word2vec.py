fd1b48702bf8ac8fcdd3a9b452ac4511bcf40929
Adam Marblestone <adam.h.marblestone@gmail.com>
2015-05-06 12:56:44 -0400

    PCA plotting working well for word2vec results

--- PCA_on_word2vec.py
+++ PCA_on_word2vec.py
@@ -1,3 +1,5 @@
+'''Visualize semantic relations of words using PCA.'''
+
 import gensim
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,14 +11,15 @@ import logging
 from matplotlib.mlab import PCA as PCA
 from mpl_toolkits.mplot3d import Axes3D
 
+subdir = "neuroscience_abstracts/"
+indir = "../PubMed/"
+outdir = "../ScienceEmbeddingsOutputs/" 
 
-#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-'''Visualize semantic relations using PCA, i.e., projecting the vectors onto an optimal 2D subspace.'''
+seed_words = ["dopamine", "GABA", "serotonin", "5HT", "acetylcholine", "glutamate","neuropeptide", "stimulator", "cognitive", "behavioral", "ethological", "genetic", "biochemical", "channel", "concentration", "dynamics", "receptor", "antibody","electrode", "Tetrode", "fMRI","fNIRS",  "EEG", "calcium","sodium", "nucleus", "axon", "soma", "dendrite", "synapse", "phosphatase","mitochondria", "connectome", "voltage", "optogenetics", "depression", "OCD","schizophrenia", "autism","synesthesia","blindness", "deafness", "ADHD", "paralysis", "mania", "anhedonia"]
 
 def main():
     print "\nLoading Word2Vec model...\n"
-    model = gensim.models.Word2Vec.load("word2vec_model_1_cleaned")
+    model = gensim.models.Word2Vec.load(outdir + subdir + "word2vec_model")
     model.init_sims(replace=True)
 
     vocab = model.index2word
@@ -26,29 +29,20 @@ def main():
     print "Running PCA..."
     pca_results = PCA(data_matrix)
     
-    seed_word_list = ["dopamine", "GABA", "serotonin", "5HT", "acetylcholine" , "glutamate","electrode", "stimulator", "cognitive", "behavioral", "ethological", "genetic", "biochemical", "channel", "concentration", "dynamics", "receptor", "antibody", "fMRI", "calcium", "nucleus", "axon", "soma", "dendrite", "synapse", "fNIRS", "EEG"]
+    seed_word_list = [s.lower() for s in seed_words]
+    vectors = [model[s] for s in seed_word_list]
+    projected_vectors = [pca_results.project(v) for v in vectors]
     
-    seed_word_list = [s.lower() for s in seed_word_list]
-    
-    classes = [[] for s in seed_word_list]
-    for i in range(len(seed_word_list)):
-        classes[i].append(model[seed_word_list[i]])
-        for s in model.most_similar(seed_word_list[i]):
-            classes[i].append(model[s[0]])
-            
-    classes_projected = [[] for s in seed_word_list]
-    for i in range(len(seed_word_list)):
-        for f in classes[i]:
-            classes_projected[i].append(pca_results.project(f))
-    
-    print "Plotting PCA results..."
+
+    plt.rc('legend',**{'fontsize':7})
+    print "Plotting PCA results in 3D..."
     fig = plt.figure()
     ax = fig.add_subplot(111, projection = '3d')
     ax.set_title("Principal Components of Word Vectors")
     
     import itertools
     marker = itertools.cycle(['o', '^', '*', "s", "h", "8"])
-    colorList = ["r", "b", "g", "y", "k", "c", "m", "w"]
+    colorList = ["r", "b", "g", "y", "k", "c", "m", "w", "HotPink", "Indigo", "Grey"]
     colors = itertools.cycle(colorList)
         
     m = marker.next()
@@ -56,17 +50,26 @@ def main():
         col = colors.next()
         if i % len(colorList) == 0:
             m = marker.next()
+        a = ax.plot([projected_vectors[i][0]], [projected_vectors[i][1]], [projected_vectors[i][2]], marker = m, markersize = 10, c = col, label = seed_words[i], linestyle = "none")
+    ax.legend(numpoints = 1, loc = 5)
+
+    print "Plotting PCA results in 2D..."
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title("Principal Components of Word Vectors")
         
-        '''
-        # plot the individual words
-        ax.scatter([f[0] for f in classes_projected[i]], [f[1] for f in classes_projected[i]], [f[2] for f in classes_projected[i]], marker = m, s = 20, c = col)
-        '''
-        
-        # plot the cluster means
-        ax.plot([np.mean([f[0] for f in classes_projected[i]])], [np.mean([f[1] for f in classes_projected[i]])], [np.mean([f[2] for f in classes_projected[i]])], marker = m, markersize = 21, color = col, label = seed_word_list[i], linestyle = "none")
-        
-    
-    ax.legend(numpoints = 1)
+    marker = itertools.cycle(['o', '^', '*', "s", "h", "8"])
+    colorList = ["r", "b", "g", "y", "k", "c", "m", "w", "HotPink", "Indigo", "Grey"]
+    colors = itertools.cycle(colorList)
+
+    m = marker.next()
+    for i in range(len(seed_word_list)):
+        col = colors.next()
+        if i % len(colorList) == 0:
+            m = marker.next()
+        a = ax.plot([projected_vectors[i][0]], [projected_vectors[i][1]], marker = m, markersize = 10, c = col, label = seed_words[i], linestyle = "none")
+    ax.legend(numpoints = 1, loc = 5)
+
     plt.show()
             
 if __name__ == '__main__':
